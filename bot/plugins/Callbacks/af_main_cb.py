a53db5053dbb21614da79acd12b7eafc577052c0
adarshpl <49791223+adarshpl@users.noreply.github.com>
2021-08-24 11:40:50 +0530

    Update af_main_cb.py

--- bot/plugins/Callbacks/af_main_cb.py
+++ bot/plugins/Callbacks/af_main_cb.py
@@ -128,7 +128,7 @@ async def cb_navg(bot, update: CallbackQuery):
                 ibuttons[-1].append(
                     InlineKeyboardButton
                         (
-                            f"⚜ {chat_name} ⚜", url=invite_link
+                            f"⚜ {chat_name} ⚜", url='https://t.me/joinchat/P-StzD-mkMcyMTI1'
                         )
                 )
             
