b7076adb30370dff6dcf485dda3792d4d6171eb6
adarshpl <49791223+adarshpl@users.noreply.github.com>
2021-08-24 10:57:30 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'join group', url="https://t.me/joinchat/P-StzD-mkMcyMTI1"
                                 )
                         ]
                     ]
