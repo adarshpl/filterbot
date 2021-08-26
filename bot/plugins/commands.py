038e8a7f754a5f39ff6ac5768d7b30949c621670
adarshpl <49791223+adarshpl@users.noreply.github.com>
2021-08-26 19:01:30 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n🚫 <s>saynotopiracy</s>",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
