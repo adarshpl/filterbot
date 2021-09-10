203ecde3b400f7743fdcf846b11a990ca907da05
adarshpl <49791223+adarshpl@users.noreply.github.com>
2021-09-10 19:30:02 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = f"{file_name} \n<a href="https://t.me/joinchat/P-StzD-mkMcyMTI1">🚫 saynotopiracy</a>"
+                caption = caption,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
